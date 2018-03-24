caa3c5eb60e01828983d9b0761f798d721e2c65f
shiv-sundar <ssundar@iastate.edu>
2018-03-24 05:00:02 -0500

    added volley library

--- app/src/main/java/hackisu/hackisuspring18/JobsService.java
+++ app/src/main/java/hackisu/hackisuspring18/JobsService.java
@@ -1,16 +1,23 @@
 package hackisu.hackisuspring18;
 
-import java.util.Scanner;
+import android.support.annotation.NonNull;
+
+import com.android.volley.*;
+import com.android.volley.toolbox.Volley;
+
 /**
  * Created by Shivkarthi on 3/24/2018.
  */
 
 public class JobsService {
+    private RequestQueue rq;
+    private String url = "https://ml.googleapis.com";
+
     public JobsService() {
 
     }
 
-    public boolean uploadData(Object obj) {
+    public boolean uploadCSV(Object obj) {
         return false;
     }
 }
