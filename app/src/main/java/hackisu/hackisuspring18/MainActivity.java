cf3a4ade3dbd505f9db3a98e4174e4efd4aa02ee
Elizabeth Li <elli1998@iastate.edu>
2018-03-23 22:44:45 -0500

    Added untracked files

--- app/src/main/java/hackisu/hackisuspring18/MainActivity.java
+++ app/src/main/java/hackisu/hackisuspring18/MainActivity.java
@@ -1,14 +1,38 @@
 package hackisu.hackisuspring18;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends Activity {
+
+    private TextView title;
+    private Button readFile;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        title = (TextView) findViewById(R.id.title);
+        readFile = (Button) findViewById(R.id.readFile);
+
+        readFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent readFileIntent = new Intent(MainActivity.this, ReadCSVActivity.class);
+                readFileIntent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                readFileIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                readFileIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                overridePendingTransition(0, 0);
+                startActivity(readFileIntent);
+            }
+
+        });
+
     }
-    //Test
 }
