8bd13e9874c087a8a76e9aadff603e7ff4dda14b
kyle <ktrom@iastate.edu>
2018-03-24 04:13:45 -0500

    APP

---
+++ app/src/main/java/hackisu/hackisuspring18/App.java
@@ -0,0 +1,42 @@
+package hackisu.hackisuspring18;
+
+
+import org.tensorflow.Graph;
+import org.tensorflow.Tensor;
+import org.tensorflow.Session;
+import java.io.FileNotFoundException;
+
+import java.io.FileReader;
+
+/**
+ * Created by Smith on 24-Mar-18.
+ */
+
+public class App {
+
+    public static void main(String args[]) {
+        System.out.println(" Hello World!");
+        String inputname = "text.txt";
+        try {
+            FileReader input = new FileReader(inputname);
+        } catch (FileNotFoundException e) {
+            System.out.println(
+                    "Unable to open file '" +
+                            fileName + "'");
+        }
+        try (Graph g = new Graph()) {
+
+
+            try (Tensor t = Tensor.create(value.getBytes("UTF-8"))) {
+                //g.opBuilder("Const", "MyConst").set
+                g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build();
+
+                try (Session s = new Session(g);
+                     Tensor output = s.runner().fetch("MyConst").run().get(0)) {
+                    System.out.println(new String(output.bytesValue(), "UTF-8"));
+                }
+            }
+        }
+    }
+}
+
